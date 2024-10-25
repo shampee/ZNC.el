8847f147e5d741bae256d47c9c2b422b9a9408f9
Hampus O <shampee@users.noreply.github.com>
2024-10-25 21:29:35 +0200

    Update znc.el

--- znc.el
+++ znc.el
@@ -4,10 +4,10 @@
 ;; Author: Yaroslav Shirokov
 ;; URL: https://github.com/sshirokov/ZNC.el
 ;; Version: 0.0.4
-;; Package-Requires: ((cl-lib "0.2"))
+;; Package-Requires: ((cl-lib >= "0.27.1"))
 ;; Also available via Marmalade http://marmalade-repo.org/
 ;;;;;;
-(require 'cl)
+(require 'cl-lib)
 (require 'erc)
 
 (defgroup znc nil
@@ -83,23 +83,23 @@ some of the quirks that arise from using it with a naive ERC. "
                          (lambda () (not (equal (current-buffer) (erc-server-buffer))))
                          (current-buffer)))))
     (if buffer
-        (loop for kidbuffer in pending
-              do (znc-kill-buffer-always kidbuffer)
-              initially (znc-kill-buffer-always buffer)
-              finally return `(buffer ,@pending))
+        (cl-loop for kidbuffer in pending
+                 do (znc-kill-buffer-always kidbuffer)
+                 initially (znc-kill-buffer-always buffer)
+                 finally return `(buffer ,@pending))
       (message "%s is unknown or not currently running"))))
 
 ;;;###autoload
 (defun znc-all (&optional disconnect)
   "Connect to all known networks"
   (interactive "P")
-  (loop for network in (znc-walk-all-servers :each 'znc-endpoint-slug)
-        do
-          (message "Connecting to: %s" network)
-          (if disconnect
-              (znc-discard network)
-            (znc-erc network))
-        collecting network))
+  (cl-loop for network in (znc-walk-all-servers :each 'znc-endpoint-slug)
+           do
+           (message "Connecting to: %s" network)
+           (if disconnect
+               (znc-discard network)
+             (znc-erc network))
+           collecting network))
 
 
 ;; Advice
@@ -133,9 +133,9 @@ some of the quirks that arise from using it with a naive ERC. "
        (znc-detach-channel))))
 
 ;;; Traversal
-(defun* znc-walk-all-servers (&key (each (lambda (&rest r) (mapcar 'identity r)))
-                                   (pred (lambda (&rest _) t))
-                                   (first nil))
+(cl-defun znc-walk-all-servers (&key (each (lambda (&rest r) (mapcar 'identity r)))
+                                     (pred (lambda (&rest _) t))
+                                     (first nil))
   "Walk every defined server and user pair calling `each' every time `pred' is non-nil
 
 Both functions are called as: (apply f slug host port user pass)
@@ -143,12 +143,12 @@ Both functions are called as: (apply f slug host port user pass)
 `pred' is a truth function
 `first' if non-nil, return the car of the result"
     (funcall (if first 'car 'identity)
-             (loop for (host port ssl users) in znc-servers
-                   appending (loop for (slug user pass) in users collecting
-                     `(,slug ,host ,port ,ssl ,user ,pass)) into endpoints
-                   finally return (loop for endpoint in endpoints
-                     if (apply pred endpoint)
-                     collect (apply each endpoint)))))
+             (cl-loop for (host port ssl users) in znc-servers
+                      appending (loop for (slug user pass) in users collecting
+				      `(,slug ,host ,port ,ssl ,user ,pass)) into endpoints
+		      finally return (loop for endpoint in endpoints
+					   if (apply pred endpoint)
+                                           collect (apply each endpoint)))))
 
 ;;; Traversal helpers
 (defun znc-walk-slugp (slug)
@@ -167,7 +167,7 @@ Both functions are called as: (apply f slug host port user pass)
 to the matching values for the endpoint"
   (let ((sympoint (gensym "endpoint")))
     `(let ((,sympoint ,endpoint))
-       (destructuring-bind (slug host port ssl user pass) ,sympoint
+       (cl-destructuring-bind (slug host port ssl user pass) ,sympoint
          ,@forms))))
 
 ;;; Helpers
